109e1072af985e6d92feab37d6173c52153251da
George Paraskevopoulos <George Paraskevopoulos>
2017-06-25 19:14:08 +0300

    Add multimodal cross validation script
    Signed-off-by: George Paraskevopoulos <George Paraskevopoulos>

---
+++ mm_crosval.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+import numpy as np
+import mmfeat.space
+
+
+
+def main():
+    text_space = mmfeat.space.Space('wiki.en.pkl')
+    # Set the parameters by cross-validation
+    tuned_parameters = [{'alpha': np.linspace(0, 1, 100)}]
+
+    print("# Tuning hyper-parameters: alpha")
+    print()
+
+    clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=5,
+                       scoring='%s_macro' % score)
+    clf.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+        print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
